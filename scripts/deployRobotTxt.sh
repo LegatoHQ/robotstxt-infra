61b6e654075c24f05aaa9b60c9483b747d8ec7d0
Roy <roy@osherove.com>
2023-02-13 12:04:03 +0200

    bin/bash anomaly

--- scripts/deployRobotTxt.sh
+++ scripts/deployRobotTxt.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+#if this fails to run, see https://stackoverflow.com/questions/21612980/running-shell-script-and-failing-with-bin-bashm-bad-interpreter-no-such-file-o
+# sed -i -e 's/\r$//' ./scripts/*.sh
+# run it with ./scripts/deployToken.sh , not with "sh scripts/deployToken.sh"
+# or https://stackoverflow.com/questions/14219092/bash-script-bin-bashm-bad-interpreter-no-such-file-or-directory
+# or https://stackoverflow.com/questions/10376206/what-is-the-preferred-bash-shebang
 source .env
 source ./scripts/deployParams.sh
 
@@ -6,4 +12,4 @@ forge create --rpc-url $MAINNET_RPC \
     --constructor-args $TOKEN_ADDRESS \
     --etherscan-api-key $ETHERSCAN_API_KEY \
     --verify \
-    src/RobotTxt.sol:RobotTxt
+    ./src/RobotTxt.sol:RobotTxt
--- scripts/updateToken.sh
+++ scripts/updateToken.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+#if this fails to run, see https://stackoverflow.com/questions/21612980/running-shell-script-and-failing-with-bin-bashm-bad-interpreter-no-such-file-o
+# sed -i -e 's/\r$//' ./scripts/*.sh
 source .env
 source ./scripts/deployParams.sh
 
